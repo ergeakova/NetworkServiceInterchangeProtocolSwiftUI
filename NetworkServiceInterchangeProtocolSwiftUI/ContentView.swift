78f7c3175143e3d67347ed5c42f5f55478506da4
ergeakova <ergeak@gmail.com>
2022-08-23 14:26:18 +0300

    Initial Commit

---
+++ NetworkServiceInterchangeProtocolSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetworkServiceInterchangeProtocolSwiftUI
+//
+//  Created by Erge Gevher Akova on 23.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkServiceInterchangeProtocolSwiftUI/NetworkServiceInterchangeProtocolSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkServiceInterchangeProtocolSwiftUIApp.swift
+//  NetworkServiceInterchangeProtocolSwiftUI
+//
+//  Created by Erge Gevher Akova on 23.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkServiceInterchangeProtocolSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
