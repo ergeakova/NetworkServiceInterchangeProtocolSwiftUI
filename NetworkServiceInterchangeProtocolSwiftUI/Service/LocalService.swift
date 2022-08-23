723e5a0e603657569003ff2bfa850776f346a1fd
ergeakova <ergeak@gmail.com>
2022-08-23 20:49:20 +0300

    Added LocalService

---
+++ NetworkServiceInterchangeProtocolSwiftUI/Service/LocalService.swift
@@ -0,0 +1,22 @@
+//
+//  LocalService.swift
+//  NetworkServiceInterchangeProtocolSwiftUI
+//
+//  Created by Erge Gevher Akova on 23.08.2022.
+//
+
+import Foundation
+
+class LocalService{
+    
+    func downloadUsers(_ resource: String) async throws -> [User]{
+        
+        guard let path = Bundle.main.path(forResource: resource, ofType: "json") else{
+            fatalError("Resource not found!!!")
+        }
+        
+        let data = try Data(contentsOf: URL(fileURLWithPath: path))
+        
+        return try JSONDecoder().decode([User].self, from: data)
+    }
+}
